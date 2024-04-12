e4bf30184951c22744f609576a1c4c278a1ab189
epic miner <155073729+epic-miner@users.noreply.github.com>
2024-04-12 13:40:19 -0700

    Add files via upload

---
+++ aman.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# Set default values
+username="user"
+password="root"
+chrome_remote_desktop_url="https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb"
+
+# Function to log messages
+log() {
+    echo "$(date +'%Y-%m-%d %H:%M:%S') - $1"
+}
+
+# Function to install packages
+install_package() {
+    package_url=$1
+    log "Downloading $package_url"
+    wget -q --show-progress "$package_url"
+    log "Installing $(basename $package_url)"
+    sudo dpkg --install $(basename $package_url)
+    log "Fixing broken dependencies"
+    sudo apt-get install --fix-broken -y
+    rm $(basename $package_url)
+}
+
+# Installation steps
+log "Starting installation"
+
+# Create user
+log "Creating user '$username'"
+sudo useradd -m "$username"
+echo "$username:$password" | sudo chpasswd
+sudo sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
+
+# Install Chrome Remote Desktop
+install_package "$chrome_remote_desktop_url"
+
+# Install XFCE desktop environment
+log "Installing XFCE desktop environment"
+sudo DEBIAN_FRONTEND=noninteractive apt-get install --assume-yes -y xfce4 desktop-base dbus-x11 xscreensaver
+
+# Set up Chrome Remote Desktop session
+log "Setting up Chrome Remote Desktop session"
+sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'
+
+# Disable lightdm service
+log "Disabling lightdm service"
+sudo systemctl disable lightdm.service
+
+#install firefox-esr
+sudo apt install firefox-esr
+
+log "Installation completed successfully"
